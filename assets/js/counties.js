f4ca2f9a8a4695bc83270838c6e93a1da71fab55
Samuel Wekesa <samuelwekesa24@gmail.com>
2026-07-04 11:46:47 +0300

    Create counties.js

---
+++ assets/js/counties.js
@@ -0,0 +1,273 @@
+// Kenya Counties and Major Towns
+
+const kenyaLocations = {
+
+    "Baringo": [
+        "Kabarnet",
+        "Eldama Ravine",
+        "Marigat",
+        "Mogotio"
+    ],
+
+    "Bomet": [
+        "Bomet",
+        "Sotik",
+        "Longisa"
+    ],
+
+    "Bungoma": [
+        "Bungoma",
+        "Webuye",
+        "Kimilili",
+        "Chwele"
+    ],
+
+    "Busia": [
+        "Busia",
+        "Malaba",
+        "Funyula"
+    ],
+
+    "Elgeyo-Marakwet": [
+        "Iten",
+        "Kapsowar",
+        "Chebara"
+    ],
+
+    "Embu": [
+        "Embu",
+        "Runyenjes",
+        "Manyatta"
+    ],
+
+    "Garissa": [
+        "Garissa",
+        "Hulugho",
+        "Ijara"
+    ],
+
+    "Homa Bay": [
+        "Homa Bay",
+        "Mbita",
+        "Oyugis"
+    ],
+
+    "Isiolo": [
+        "Isiolo",
+        "Garbatulla"
+    ],
+
+    "Kajiado": [
+        "Kajiado",
+        "Ngong",
+        "Kitengela",
+        "Ongata Rongai"
+    ],
+
+    "Kakamega": [
+        "Kakamega",
+        "Mumias",
+        "Lugari"
+    ],
+
+    "Kericho": [
+        "Kericho",
+        "Litein",
+        "Londiani"
+    ],
+
+    "Kiambu": [
+        "Ruiru",
+        "Thika",
+        "Kiambu",
+        "Limuru",
+        "Juja",
+        "Kikuyu",
+        "Githunguri"
+    ],
+
+    "Kilifi": [
+        "Kilifi",
+        "Malindi",
+        "Watamu"
+    ],
+
+    "Kirinyaga": [
+        "Kerugoya",
+        "Sagana",
+        "Kutus"
+    ],
+
+    "Kisii": [
+        "Kisii",
+        "Ogembo",
+        "Suneka"
+    ],
+
+    "Kisumu": [
+        "Kisumu",
+        "Ahero",
+        "Maseno"
+    ],
+
+    "Kitui": [
+        "Kitui",
+        "Mwingi",
+        "Mutomo"
+    ],
+
+    "Kwale": [
+        "Ukunda",
+        "Kwale"
+    ],
+
+    "Laikipia": [
+        "Nanyuki",
+        "Nyahururu"
+    ],
+
+    "Machakos": [
+        "Machakos",
+        "Athi River",
+        "Mlolongo",
+        "Kangundo"
+    ],
+
+    "Makueni": [
+        "Wote",
+        "Mtito Andei"
+    ],
+
+    "Mandera": [
+        "Mandera"
+    ],
+
+    "Marsabit": [
+        "Marsabit",
+        "Moyale"
+    ],
+
+    "Meru": [
+        "Meru",
+        "Maua",
+        "Nkubu"
+    ],
+
+    "Migori": [
+        "Migori",
+        "Awendo",
+        "Kehancha"
+    ],
+
+    "Mombasa": [
+        "Mombasa",
+        "Likoni",
+        "Nyali",
+        "Bamburi"
+    ],
+
+    "Murang'a": [
+        "Murang'a",
+        "Kenol",
+        "Kangema"
+    ],
+
+    "Nairobi": [
+        "CBD",
+        "Westlands",
+        "Embakasi",
+        "Kasarani",
+        "Karen",
+        "Kilimani",
+        "Lang'ata",
+        "Roysambu"
+    ],
+
+    "Nakuru": [
+        "Nakuru",
+        "Naivasha",
+        "Molo",
+        "Gilgil"
+    ],
+
+    "Nandi": [
+        "Kapsabet",
+        "Nandi Hills"
+    ],
+
+    "Narok": [
+        "Narok",
+        "Kilgoris"
+    ],
+
+    "Nyamira": [
+        "Nyamira",
+        "Keroka"
+    ],
+
+    "Nyandarua": [
+        "Ol Kalou",
+        "Engineer"
+    ],
+
+    "Nyeri": [
+        "Nyeri",
+        "Karatina",
+        "Othaya"
+    ],
+
+    "Samburu": [
+        "Maralal"
+    ],
+
+    "Siaya": [
+        "Siaya",
+        "Bondo",
+        "Ugunja"
+    ],
+
+    "Taita Taveta": [
+        "Voi",
+        "Taveta",
+        "Wundanyi"
+    ],
+
+    "Tana River": [
+        "Hola"
+    ],
+
+    "Tharaka-Nithi": [
+        "Chuka",
+        "Kathwana"
+    ],
+
+    "Trans Nzoia": [
+        "Kitale",
+        "Kiminini"
+    ],
+
+    "Turkana": [
+        "Lodwar"
+    ],
+
+    "Uasin Gishu": [
+        "Eldoret",
+        "Burnt Forest"
+    ],
+
+    "Vihiga": [
+        "Mbale",
+        "Luanda"
+    ],
+
+    "Wajir": [
+        "Wajir"
+    ],
+
+    "West Pokot": [
+        "Kapenguria"
+    ]
+
+};
+
+export default kenyaLocations;
