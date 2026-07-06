e886bd003dbda24e38eecdc1769d522e33f068fa
Samuel Wekesa <samuelwekesa24@gmail.com>
2026-07-06 19:52:53 +0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    if (loader) {
+
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+
+            loader.style.display = "none";
+
+        }, 400);
+
+    }
+
+});
